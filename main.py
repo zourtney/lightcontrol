30172b82a15ad12dd3c8790dc07a3f10138553e7
Courtney Christensen <courtney.christensen@isirona.com>
2013-08-21 14:14:02 -0500

    Added batch GET and PUT

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import json
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, make_response
 from gpiocrust import Header, OutputPin
 
 # Set up Raspberry Pi I/O
@@ -19,22 +19,58 @@ app = Flask(__name__, static_folder='templates', static_url_path='')
 app.debug = True
 
 
-"""
-
-API
 
 """
-@app.route('/outlet/<num>/', methods=['GET'])
+
+Single outlet API
+
+"""
+def serialize_outlet(num):
+  outlet = out[int(num)]
+  return {
+    'id': int(num),
+    'value': outlet.value
+  }
+
+@app.route('/outlets/<num>/', methods=['GET'])
 def get_out(num):
-  pin = out[int(num)]
-  return jsonify(id=int(num), value=pin.value)
+  return jsonify(serialize_outlet(num))
 
-@app.route('/outlet/<num>/', methods=['PUT'])
+@app.route('/outlets/<num>/', methods=['PUT'])
 def set_out(num):
   pin = out[int(num)]
   requestJson = json.loads(request.data)
   pin.value = int(requestJson['value'])
-  return jsonify(id=int(num), value=pin.value)
+  return jsonify(serialize_outlet(num))
+
+
+
+"""
+
+Batch API
+
+"""
+def serialize_all():
+  return [
+    serialize_outlet(1),
+    serialize_outlet(2),
+    serialize_outlet(3),
+    serialize_outlet(4)
+  ]
+
+@app.route('/outlets/', methods=['GET'])
+def get_all():
+  resp = make_response(json.dumps(serialize_all()))
+  resp.mimetype = 'application/json'
+  return resp
+
+@app.route('/outlets/', methods=['PUT'])
+def set_all():
+  for outlet in json.loads(request.data):
+    out[int(outlet['id'])].value = outlet['value']
+  resp = make_response(json.dumps(serialize_all()))
+  resp.mimetype = 'application/json'
+  return resp
 
 
 
